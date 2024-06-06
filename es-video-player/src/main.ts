3c6f41453250f45c5627491f589467c1282c0490
liulipeng <liulipeng@huan.tv>
2024-06-06 12:59:44 +0800

    feat: es-video-player

---
+++ es-video-player/src/main.ts
@@ -0,0 +1,35 @@
+import {ESApp} from '@extscreen/es3-vue'
+import application from './App.vue'
+
+import routes from './routes'
+
+const routerOptions = {
+  main: 'home',
+  error: 'error',
+  limit: 10,
+  routes: routes
+}
+
+import {createESApp} from '@extscreen/es3-core'
+
+const app: ESApp = createESApp(application, routerOptions)
+
+import {ESComponent} from '@extscreen/es3-component'
+
+app.use(ESComponent)
+
+import '@quicktvui/quicktvui3/dist/index.css'
+import {QuickTVUI} from '@quicktvui/quicktvui3'
+
+app.use(QuickTVUI)
+
+
+import {createESPlayer} from "@extscreen/es3-player";
+
+const player = createESPlayer();
+app.use(player);
+
+import {createESVideoPlayer} from "@extscreen/es3-video-player";
+
+const videoPlayer = createESVideoPlayer();
+app.use(videoPlayer);
